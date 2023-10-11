02ad2cc35636f2c54467793ae7a4cf0b62173a45
unknown <alu0101392561@ull.edu.es>
2023-10-11 14:12:43 +0100

    Scripts actualizados

--- scripts/ejercicio12_P3.cs
+++ scripts/ejercicio12_P3.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ejercicio12_P3 : MonoBehaviour
 {
+    public Vector3 moveDirection;
+    public float speed;
+    public GameObject find_object;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,30 @@ public class ejercicio12_P3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");        
+        if (Input.GetKeyDown(KeyCode.UpArrow) == true) {
+            moveDirection = new Vector3(0, verticalMovement + speed, 0);
+            transform.Translate(moveDirection);
+            Debug.Log("Pulsé UpArrow");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) == true) {
+            moveDirection = new Vector3(0, verticalMovement - speed, 0);
+            transform.Translate(moveDirection);
+            Debug.Log("Pulsé DownArrow");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) == true) {
+            moveDirection = new Vector3(horizontalMovement - speed, 0, 0);
+            transform.Translate(moveDirection);
+            Debug.Log("Pulsé LeftArrow");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) == true) {
+            moveDirection = new Vector3(horizontalMovement + speed, 0, 0);
+            transform.Translate(moveDirection);
+            Debug.Log("Pulsé RightArrow");
+        }
+        Vector3 direcctionToMove = (find_object.transform.position - transform.position).normalized; // nos quedamos con la posición del objeto a buscar
+        moveDirection = direcctionToMove * speed * Time.deltaTime;
+        transform.Translate(moveDirection);
     }
 }
